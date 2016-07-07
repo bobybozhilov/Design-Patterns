a684d3d70fc8b04c0eb747e4689ea7d805bae7c1
bobybozhilov <bobybozhilov@abv.bg>
2016-07-07 19:02:22 +0300

    go

---
+++ DesignPatterns/Design Patterns 5 Factory Pattern/src/EnemyShip.java
@@ -0,0 +1,49 @@
+
+public abstract class EnemyShip {
+
+	private String name;
+	private double ammountOfDamage;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the ammountDamage
+	 */
+	public double getAmmountOfDamage() {
+		return ammountOfDamage;
+	}
+
+	/**
+	 * @param ammountDamage
+	 *            the ammountDamage to set
+	 */
+	public void setAmmountDamage(double ammountOfDamage) {
+		this.ammountOfDamage = ammountOfDamage;
+	}
+
+	public void followHeroShip(){
+		System.out.println(getName()+" is following the hero");
+	}// end followHeroShip
+
+	public void displayEnemyShip(){
+		System.out.println(getName()+" is on the screen");
+	}// end displayEnemyShip
+	
+	public void enemyShipShoots(){
+		System.out.println(getName()+" attacks and does "+getAmmountOfDamage());
+	}// end displayEnemyShip
+
+}
